560c6ca10756e2754216874404c0d42eacc5806c
hd-desktop <hungrysoul9@gmail.com>
2019-10-08 17:21:37 +0900

    [enum class] 상속을 받아 사이즈 변경을 할 수 있다

--- enum-class/enum-class.cpp
+++ enum-class/enum-class.cpp
@@ -8,10 +8,35 @@ enum class eColor
 	Blue
 };
 
+enum class eUint8 : unsigned char
+{
+	One,
+	Two
+};
 
-int main()
+enum class eUint64 : unsigned __int64
+{
+	One,
+	Two
+};
+
+
+void print_sizeof()
 {
 	eColor color = eColor::Red;
 	std::cout << "sizeof( eColor ): " << sizeof( eColor ) << std::endl;
 	std::cout << "sizeof( color ): " << sizeof( color ) << std::endl;
+
+	eUint8 euint8 = eUint8::One;
+	std::cout << "sizeof( eUint8 ): " << sizeof( eUint8 ) << std::endl;
+	std::cout << "sizeof( euint64 ): " << sizeof( euint8 ) << std::endl;
+
+	eUint64 euint64 = eUint64::One;
+	std::cout << "sizeof( eUint64 ): " << sizeof( eUint64 ) << std::endl;
+	std::cout << "sizeof( euint64 ): " << sizeof( euint64 ) << std::endl;
+}
+
+int main()
+{
+	print_sizeof();
 }
