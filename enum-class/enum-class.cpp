21f70e254da298a963c24b0f59caf725d119d9d4
hd-desktop <hungrysoul9@gmail.com>
2019-10-08 17:16:35 +0900

    [enum class] 사이즈에 대해 알아봅시다
    - x86/x64 동일하게 4바이트

---
+++ enum-class/enum-class.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+
+enum class eColor
+{
+	Red,
+	Green = 1,
+	Blue
+};
+
+
+int main()
+{
+	eColor color = eColor::Red;
+	std::cout << "sizeof( eColor ): " << sizeof( eColor ) << std::endl;
+	std::cout << "sizeof( color ): " << sizeof( color ) << std::endl;
+}
