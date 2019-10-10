8dcacaf3de76454e2cbaded8bea0c09bb0da3fb2
hd-desktop <hungrysoul9@gmail.com>
2019-10-10 13:38:13 +0900

    [shared_ptr] raw 포인터와의 연관성

---
+++ shared_ptr/shared_ptr.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <atomic>
+#include <memory>
+
+
+std::atomic<int> g_key = 0;
+
+
+struct client_socket
+{
+	//client_socket() : key( ++g_key ) {}
+
+	int key{ ++g_key };
+};
+
+
+void delete_first_shared_ptr()
+{
+	std::cout << __FUNCTION__ << std::endl;
+
+	client_socket* rawp = new client_socket;
+	std::cout << "rawp.key: " << rawp->key << std::endl;
+
+	std::shared_ptr<client_socket> sp;// = std::make_shared<client_socket>( rawp );
+	sp.reset( rawp );
+	std::cout << "sp.key: " << sp->key << std::endl;
+
+	rawp->key++;
+	std::cout << "rawp.key: " << rawp->key << std::endl;
+	std::cout << "sp.key: " << sp->key << std::endl;
+
+	sp = nullptr;
+	std::cout << "rawp.key: " << rawp->key << std::endl;
+	std::cout << std::endl;
+}
+
+
+void delete_first_raw_pointer()
+{
+	std::cout << __FUNCTION__ << std::endl;
+
+	client_socket* rawp = new client_socket;
+	std::cout << "rawp.key: " << rawp->key << std::endl;
+
+	std::shared_ptr<client_socket> sp;// = std::make_shared<client_socket>( rawp );
+	sp.reset( rawp );
+	std::cout << "sp.key: " << sp->key << std::endl;
+
+	rawp->key++;
+	std::cout << "rawp.key: " << rawp->key << std::endl;
+	std::cout << "sp.key: " << sp->key << std::endl;
+
+	delete rawp;
+	std::cout << "sp.key: " << sp->key << std::endl;
+	std::cout << std::endl;
+}
+
+
+int main()
+{
+	delete_first_shared_ptr();
+	delete_first_raw_pointer();
+}
