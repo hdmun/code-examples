79ed9f9d97784a7d96a313a35222cff2f0e0f341
hd-desktop <hungrysoul9@gmail.com>
2019-10-11 15:17:36 +0900

    [stl/list] iterator 저장해서 쓰기
    - list 는 iterator 객체를 따로 저장해서 쓸 수 있다.
    - 당연하게도 저장한 iterator에 해당하는 list 요소를 쓰면 죽는다.
    - 이거 체크를 어떻게 하는지 알아보자.

---
+++ list/list.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <atomic>
+#include <list>
+
+#include <cassert>
+
+
+std::atomic<int> g_key = 0;
+
+struct foo
+{
+	int member{ ++g_key };
+};
+
+
+void store_list_iter()
+{
+	std::list<foo> list_;
+	for ( int i = 0; i < 5; i++ ) {
+		list_.push_back( foo() );
+	}
+
+	std::list<foo>::iterator it_store = list_.end();
+
+	it_store--;
+	std::cout << "list_.size: " << list_.size() << ", it_store->member: " << it_store->member << std::endl;
+
+	list_.remove_if( []( auto& elem ) {
+		return elem.member == 2;
+	} );
+	std::cout << "list_.size: " << list_.size() << ", it_store->member: " << it_store->member << std::endl;
+
+	// 이러면 죽는다.
+// 	list_.clear();
+// 	assert( it_store == list_.end() );
+
+	// 이래도 죽는다.
+	list_.remove_if( [it_store]( auto& elem ) {
+		return elem.member == it_store->member;
+	} );
+	std::cout << "list_.size: " << list_.size() << ", it_store->member: " << it_store->member << std::endl;
+}
+
+
+int main()
+{
+	store_list_iter();
+}
