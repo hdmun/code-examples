a3164c1dd7cbb9f063c3258741c37d3e0b931728
hdmun <hdmun@nexon.co.kr>
2019-10-16 16:30:34 +0900

    [winapi/NamedPipe] 네임드 파이프 예제
    - 단방향 전송만 처리함
    - 재연결 안 됨

---
+++ windows-api/NamedPipe/NamedPipeClient/NamedPipeClient.cpp
@@ -0,0 +1,53 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+	std::cout << "NamedPipeClient" << std::endl;
+
+	HANDLE hPipe = ::CreateFile(
+		TEXT( "\\\\.\\pipe\\WzAdminStalker" )
+		, GENERIC_READ | GENERIC_WRITE
+		, 0
+		, NULL
+		, OPEN_EXISTING
+		, 0
+		, NULL
+	);
+	std::cout << "hPipe " << hPipe << std::endl;
+	if ( hPipe == INVALID_HANDLE_VALUE ) {
+		system( "pause" );
+		return 1;
+	}
+
+	DWORD dwMode = PIPE_READMODE_MESSAGE | PIPE_WAIT;
+	BOOL bRet = ::SetNamedPipeHandleState( hPipe, &dwMode, NULL, NULL );
+	std::cout << "bRet " << bRet << std::endl;
+	if ( !bRet ) {
+		system( "pause" );
+		return 1;
+	}
+
+	TCHAR message[ MAX_PATH ] = TEXT("Message from Client");
+	DWORD dwSendSize = 0;
+	while ( true ) {
+		BOOL bWrite = ::WriteFile(
+			hPipe
+			, message
+			, MAX_PATH * sizeof( TCHAR )
+			, &dwSendSize
+			, NULL
+		);
+		if ( !bWrite || dwSendSize == 0 ) {
+			::Sleep( 100 );
+			continue;
+		}
+
+		std::cout << "bWrite " << bWrite << ", dwSendSize " << dwSendSize << std::endl;
+	}
+
+	::CloseHandle( hPipe );
+	return 0;
+}
---
+++ windows-api/NamedPipe/NamedPipeServer/NamedPipeServer.cpp
@@ -0,0 +1,65 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <iostream>
+#include <tchar.h>
+
+int main()
+{
+	std::cout << "NamedPipeServer" << std::endl;
+
+	constexpr LPCTSTR lpName = TEXT( "\\\\.\\pipe\\WzAdminStalker" );
+	constexpr DWORD dwOpenMode = PIPE_ACCESS_DUPLEX;
+	constexpr DWORD dwPipeMode = PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT;
+	constexpr DWORD nMaxInstances = 1;
+	constexpr DWORD nOutBufferSize = 65536;
+	constexpr DWORD nInBufferSize = nOutBufferSize;
+	constexpr DWORD nDefaultTimeOut = 0;
+	constexpr LPSECURITY_ATTRIBUTES lpSecurityAttributes = nullptr;
+	HANDLE hPipe = ::CreateNamedPipe(
+		lpName
+		, dwOpenMode
+		, dwPipeMode
+		, nMaxInstances
+		, nOutBufferSize
+		, nInBufferSize
+		, nDefaultTimeOut
+		, lpSecurityAttributes
+	);
+	std::cout << "hPipe " << hPipe << std::endl;
+	if ( hPipe == INVALID_HANDLE_VALUE ) {
+		system( "pause" );
+		return 1;
+	}
+
+	BOOL bConnect = ::ConnectNamedPipe( hPipe, nullptr );
+	std::cout << "bConnect " << bConnect << std::endl;
+	if ( !bConnect ) {
+		system( "pause" );
+		return 1;
+	}
+
+	while ( true ) {
+		TCHAR recv[ MAX_PATH ] = { 0, };
+		DWORD dwRecvSize = 0;
+		BOOL bRead = ::ReadFile(
+			hPipe
+			, recv
+			, MAX_PATH * sizeof( TCHAR )
+			, &dwRecvSize
+			, NULL );
+
+		if ( !bRead || dwRecvSize == 0 ) {
+			::Sleep( 100 );
+			continue;
+		}
+
+		std::cout << "dwRecvSize " << dwRecvSize << std::endl;
+
+		recv[ dwRecvSize / sizeof( TCHAR ) - 1 ] = TEXT( '\x00' );
+		_tprintf( TEXT( "Recv Message : %s \n" ), recv );
+	}
+
+	::CloseHandle( hPipe );
+	return 0;
+}
