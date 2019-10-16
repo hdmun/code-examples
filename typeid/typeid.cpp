9e0bd4f82cd769441b0f9a77f6541815eb0d6795
hdmun <hdmun@nexon.co.kr>
2019-10-16 14:09:20 +0900

    [typeid] 사용 방법

---
+++ typeid/typeid.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+class foo
+{
+public:
+	const char* class_name() { return typeid( this ).name(); }
+};
+
+int main()
+{
+	foo foo;
+	const char* name = foo.class_name();
+	std::cout << name << std::endl;
+}
