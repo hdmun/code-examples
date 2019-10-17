2ee333610c1526be6fe6a4179750c5a45b881d10
hd-desktop <hungrysoul9@gmail.com>
2019-10-17 17:01:44 +0900

    [stl/string] c_str() 함수

---
+++ stl/string/string.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+namespace c_str {
+
+	std::string asNull()
+	{
+		return "";
+	}
+
+	std::string asBool(bool is)
+	{
+		return is ? "true" : "false";
+	}
+
+	std::string asString()
+	{
+		return std::string("string c_str test");
+	}
+
+	void run()
+	{
+		const char* s = nullptr;
+
+		s = asNull().c_str();
+		std::cout << s << std::endl;
+
+		s = asBool(true).c_str();
+		std::cout << s << std::endl;
+
+		s = asBool(false).c_str();
+		std::cout << s << std::endl;
+
+		s = asString().c_str();
+		std::cout << s << std::endl;
+	}
+}
+
+int main()
+{
+	c_str::run();
+}
