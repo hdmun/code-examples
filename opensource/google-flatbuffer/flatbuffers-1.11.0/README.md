8cbd0c33d4516b5e00f764f4c9ff08fb4ce9ceb8
hd-desktop <hungrysoul9@gmail.com>
2019-10-20 17:43:18 +0900

    [opensource/google-flatbuffer] 플랫버퍼 직렬화/역직렬화 코드

---
+++ opensource/google-flatbuffer/flatbuffers-1.11.0/README.md
@@ -0,0 +1,6 @@
+# google flatbuffers-1.11.0
+
+https://github.com/google/flatbuffers/releases
+
+include 파일만 추가함
+
---
+++ opensource/google-flatbuffer/google-flatbuffer.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "protocol/user_generated.h"
+
+uint8_t* serialize(flatbuffers::FlatBufferBuilder& fbb)
+{
+	auto fbb_offset = protocol::CreateuserDirect(fbb, "id_data", "name_data");
+	fbb.Finish(fbb_offset);
+	return fbb.GetBufferPointer();
+}
+
+void deserialize(const uint8_t* buf)
+{
+	const protocol::user* userData = protocol::Getuser(buf);
+	std::cout << "deserialize " << userData->name()->str() << ", " << userData->id()->str() << std::endl;
+}
+
+int main()
+{
+	flatbuffers::FlatBufferBuilder fb;
+	uint8_t* buf = serialize(fb);
+	
+	deserialize(buf);
+}
