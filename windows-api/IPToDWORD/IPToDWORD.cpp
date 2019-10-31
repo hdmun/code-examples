ba4ddccc4368fca0ed257f6baf95582e5b2ec6f6
hdmun <hdmun@nexon.co.kr>
2019-10-31 10:30:19 +0900

    [winapi/winsock2] inet_pton 사용법

---
+++ windows-api/IPToDWORD/IPToDWORD.cpp
@@ -0,0 +1,17 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#include <WS2tcpip.h>
+
+#include <cstdio>
+
+
+int main()
+{
+	const char* ip = "127.0.0.1";
+	in_addr addr = { 0, };
+
+	INT nRet = ::inet_pton( AF_INET, ip, &addr.s_addr );
+	printf( "%lu\n", addr.s_addr );
+}
