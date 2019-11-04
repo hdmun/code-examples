2921c084c59aa4d29f2fbfac98c857c9f6dbb01a
hd-desktop <hungrysoul9@gmail.com>
2019-11-04 09:47:20 +0900

    [constructor] 인스턴스 if 조건문에 사용하기
    - bool 연산자 오버로딩하면 됨

---
+++ constructor/constructor.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+
+class Foo
+{
+public:
+	Foo() {
+		std::cout << "Foo()" << std::endl;
+	}
+	virtual ~Foo() {
+		std::cout << "~Foo()" << std::endl;
+	}
+
+	operator bool() { return true; }
+};
+
+
+int main()
+{
+	if ( auto f = Foo() ) {
+		std::cout << "inside if ( Foo() ) block" << std::endl;
+	}
+
+	std::cout << "end of main" << std::endl;
+	return 0;
+}
