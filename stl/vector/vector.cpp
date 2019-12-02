af54b65a15c41b30a76e4b414f2270ad5eb2419f
hd-desktop <hungrysoul9@gmail.com>
2019-12-02 09:55:58 +0900

    [stl/vector] reserve 후 push_back

---
+++ stl/vector/vector.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+void push_back_after_reserve()
+{
+	std::vector<int> v;
+	for ( int i = 0; i < 5; i++ ) {
+		v.push_back( i );
+	}
+
+	std::cout << "v.size(): " << v.size() << std::endl;
+	std::cout << "v.capacity(): " << v.capacity() << std::endl;
+
+	v.reserve( v.size() * 2 );
+
+	for ( int i = 5; i < 10; i++ ) {
+		v.push_back( i );
+	}
+
+	std::cout << "v.size(): " << v.size() << std::endl;
+	std::cout << "v.capacity(): " << v.capacity() << std::endl;
+	for ( auto& iter : v ) {
+		std::cout << iter << std::endl;
+	}
+}
+
+int main()
+{
+	push_back_after_reserve();
+}
