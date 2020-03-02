58e340f147dbf3555fb42f8e7bbcbd31ed16b7b5
hdmun <hdmun@nexon.co.kr>
2020-03-02 10:01:03 +0900

    [winapi/Reg~] 레지스트리 관련 코드
    - 쓰다 만거 같은데 일단 커밋

---
+++ windows-api/RegQueryValue/RegQueryValue.cpp
@@ -0,0 +1,45 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <iostream>
+#include <vector>
+
+
+HKEY _RegCreateKeyExW()
+{
+	LPCWSTR lpSubKey = L"SOFTWARE\RegTest";
+	DWORD Reserved = 0;
+	LPWSTR lpClass = nullptr;
+	DWORD dwOption = REG_OPTION_NON_VOLATILE;
+	REGSAM samDesired = ( KEY_CREATE_SUB_KEY | KEY_ENUMERATE_SUB_KEYS | KEY_QUERY_VALUE | KEY_SET_VALUE );
+	SECURITY_ATTRIBUTES secAttr = { 0, };
+
+	HKEY hKey = nullptr;
+	DWORD dwDisposition = 0;
+	LSTATUS ret = ::RegCreateKeyExW( HKEY_LOCAL_MACHINE, lpSubKey, Reserved, lpClass, dwOption, samDesired, nullptr, &hKey, &dwDisposition );
+
+	std::cout << "RegCreateKeyExW: " << ret << std::endl;
+	return hKey;
+}
+
+
+int main()
+{
+	HKEY hKey = _RegCreateKeyExW();
+
+	LPCWSTR lpValueName = L"REG_BINARY_TEST";
+	DWORD dwReserved = 0;
+	DWORD dwType = REG_BINARY;
+	DWORD dwLen = 0;
+	LSTATUS ret = ::RegQueryValueExW( hKey, lpValueName, &dwReserved, &dwType, nullptr, &dwLen );
+	if ( ret != ERROR_SUCCESS || dwType != REG_BINARY || dwLen == 0 ) {
+		return;
+	}
+
+	std::vector<BYTE> aData;
+	aData.resize( dwLen );
+	ret = ::RegQueryValueExW( hKey, lpValueName, &dwReserved, &dwType, aData.data(), &dwLen );
+	if ( ret != ERROR_SUCCESS ) {
+		aData.clear();
+	}
+}
---
+++ windows-api/RegSetValue/RegSetValue.cpp
@@ -0,0 +1,47 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+HKEY _RegCreateKeyExW()
+{
+	LPCWSTR lpSubKey = L"SOFTWARE\RegTest";
+	DWORD Reserved = 0;
+	LPWSTR lpClass = nullptr;
+	DWORD dwOption = REG_OPTION_NON_VOLATILE;
+	static const REGSAM samDesired = ( KEY_CREATE_SUB_KEY | KEY_ENUMERATE_SUB_KEYS | KEY_QUERY_VALUE | KEY_SET_VALUE );
+	SECURITY_ATTRIBUTES secAttr = { 0, };
+
+	HKEY hKey = nullptr;
+	DWORD dwDisposition = 0;
+	LSTATUS ret = ::RegCreateKeyExW( HKEY_LOCAL_MACHINE, lpSubKey, Reserved, lpClass, dwOption, samDesired, nullptr, &hKey, &dwDisposition );
+	if ( ret == ERROR_SUCCESS ) {
+		__noop;
+	}
+
+	std::cout << "RegCreateKeyExW: " << ret << std::endl;
+	return hKey;
+}
+
+
+int main()
+{
+	HKEY hKey = _RegCreateKeyExW();
+
+	LPCWSTR lpValueName = L"REG_BINARY_TEST";
+	DWORD Reserved = 0;
+	DWORD dwType = REG_BINARY;
+
+	DWORD cbSize = sizeof( WCHAR ) * std::wcslen( lpValueName );
+	std::vector< BYTE > buffer;
+	buffer.resize( cbSize );
+
+	LSTATUS ret = ::RegSetValueExW( hKey, lpValueName, Reserved, dwType, buffer.data(), cbSize );
+	if ( ret == ERROR_SUCCESS ) {
+		__noop;
+	}
+	std::cout << "RegSetValueExW: " << ret << ", " << std::endl;
+}
