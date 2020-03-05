4cfdb8ba8b173a39029cab415bfc025426a2ab00
hdmun <hdmun@nexon.co.kr>
2020-03-05 14:06:02 +0900

    [winapi/CheckRemote] 리모트 세션 체크 - RDP 세션인지 체크한다

---
+++ windows-api/CheckRemote/CheckRemote.cpp
@@ -0,0 +1,30 @@
+﻿// CheckRemote.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+BOOL IsRemoteSession( void )
+{
+	return GetSystemMetrics( SM_REMOTESESSION );
+}
+
+BOOL IsRemoteControl( void )
+{
+	return GetSystemMetrics( SM_REMOTECONTROL );
+}
+
+int main()
+{
+	while ( true ) {
+		std::cout << "IsRemoteSession: " << IsRemoteSession();
+		std::cout << ", IsRemoteControl: " << IsRemoteControl();
+		std::cout << std::endl;
+		Sleep( 1000 );
+	}
+
+	return 0;
+}
