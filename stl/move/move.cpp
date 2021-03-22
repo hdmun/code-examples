7d1284130d23904e2f83709d03c98e4855934667
desktop <>
2021-03-22 18:37:34 +0900

    [stl/move] list move 테스트 코드, struct 버전

--- stl/move/move.cpp
+++ stl/move/move.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <list>
 
+static int g_id = 1;
 
 class foo {
 public:
@@ -10,17 +11,41 @@ public:
 	virtual ~foo() {
 		std::cout << "~foo()" << std::endl;
 	}
+
+	int id{ g_id++ };
 };
 
+void test_move_list_unique_ptr()
+{
+	std::list< std::unique_ptr< foo > > l1;
+	for (int i = 0; i < 5; i++) {
+		l1.push_back(std::make_unique< foo >());
+	}
+
+	std::list< std::unique_ptr< foo > > l2;
+	// std::move(l1.begin(), l1.end(), std::move_backward(l2));
+
+	std::cout << "test_move_list_unique_ptr: l1.size(): " << l1.size() << ", " << "l2.size(): " << l2.size() << std::endl;
+}
+
+void test_move_struct_to_list()
+{
+	std::cout << __FUNCTION__ << std::endl;
+
+	foo foo1;
+
+	std::list<foo> l1 = { foo() };
+	l1.push_back(std::move(foo1));
+	for (auto& e : l1) {
+		std::cout << e.id << std::endl;
+	}
+
+	std::cout << foo1.id << std::endl;
+}
 
 int main()
 {
-	std::list< std::unique_ptr< foo > > l1;
-	l1.push_back( std::make_unique< foo >() );
-
-	std::list< std::unique_ptr< foo > > l2;
-	std::move( l1.begin(), l1.end(), std::move_backward( l2 ) );
-
-	std::cout << "l1.size(): " << l1.size() << ", " << "l2.size(): " << l2.size() << std::endl;
+	// test_move_list_unique_ptr();
+	test_move_struct_to_list();
 	return 0;
 }
