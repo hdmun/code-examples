fde43a9bb27ef12c1652a1d13cd83d71d36171e8
hdmun <huidae.mun@gmail.com>
2021-09-02 16:38:42 +0900

    [stl/filesystem] v140, filesystem::path

---
+++ stl/filesystem/filesystem.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <filesystem>
+
+
+namespace fs = std::experimental::filesystem;
+
+int main()
+{
+    fs::path path = "root";
+    path /= "\\dir";
+    std::cout << path << std::endl;  // root\dir
+
+    path /= "child";
+    std::cout << path << std::endl;  // root\dir\child
+
+    path.append("child2");
+    std::cout << path << std::endl;  // root\dir\child\child2
+
+    path.append("\\child3");
+    std::cout << path << std::endl;  // root\dir\child\child2\child3
+    return 0;
+}
