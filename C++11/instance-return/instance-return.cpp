2049047e38cf23ba0583f7bc6c68e7eb8f6b3dd4
hdmun <huidae.mun@gmail.com>
2021-09-09 20:13:45 +0900

    지역 객체 리턴 코드 - 코드 로직 및 최적화에 따라 다르지만 이러면 소멸자가 불려질 가능성이 있다 - 기본적인거지만 코드, 자료구조 복잡도가 증가하면 이런 케이스가 생길 가능성이 있음

---
+++ C++11/instance-return/instance-return.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <memory>
+
+
+int g_sn = 0;
+bool g_echo = false;
+
+class Foo {
+public:
+    Foo() : sn(++g_sn) {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+    }
+
+    virtual ~Foo() {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+    }
+
+    int sn;
+};
+
+
+class Array {
+public:
+    Array() {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+    }
+
+    Array(std::vector<std::shared_ptr<Foo>> elem) : elem(elem) {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+    }
+
+    virtual ~Array() {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+
+        elem.clear();
+    }
+
+    void push_back(std::shared_ptr<Foo> foo) {
+        elem.push_back(foo);
+    }
+
+    void print() {
+        for (auto& item : elem) {
+            std::cout << item->sn << std::endl;
+        }
+    }
+
+private:
+    std::vector<std::shared_ptr<Foo>> elem;
+};
+
+
+class Map {
+public:
+    Map() {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+    }
+
+    virtual ~Map() {
+        if (g_echo) std::cout << __FUNCTION__ << std::endl;
+    }
+
+    void push(const char* key, std::shared_ptr<Foo> foo) {
+        auto iter = map.find(key);
+        if (iter == map.end()) {
+            Array arr;
+            arr.push_back(foo);
+            map.insert(std::make_pair(key, arr));
+        }
+        else {
+            iter->second.push_back(foo);
+        }
+    }
+
+    Array get(const char* key) {
+        Array arr;
+        auto iter = map.find(key);
+        if (iter == map.end()) {
+            return arr;
+        }
+        return iter->second;
+    }
+
+private:
+    std::map<const char*, Array> map;
+};
+
+
+Map g_map;
+
+Array getKey(const char* key)
+{
+    if (g_echo) std::cout << "start " << __FUNCTION__ << std::endl << std::endl;
+
+    Array arr;
+    arr = g_map.get(key);
+    
+    if (g_echo) std::cout << std::endl << "end " << __FUNCTION__ << std::endl << std::endl;
+
+    return arr;  // ~Array
+}
+
+
+int main()
+{
+    g_map.push("key-1", std::make_shared<Foo>(Foo()));
+    g_map.push("key-1", std::make_shared<Foo>(Foo()));
+    g_map.push("key-2", std::make_shared<Foo>(Foo()));
+
+    g_echo = true;
+
+    auto arr = getKey("key-1");
+
+    if (g_echo) std::cout << "after arr.print(); " << std::endl;
+
+    arr.print();
+
+    g_echo = false;
+    return 0;
+}
