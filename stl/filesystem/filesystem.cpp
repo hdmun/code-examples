799c1fb6d3d5ec372b64290c8ea959aaf6dcf3ab
hdmun <huidae.mun@gmail.com>
2021-09-27 08:56:59 +0900

    [stl/filesystem] 사용법 추가

--- stl/filesystem/filesystem.cpp
+++ stl/filesystem/filesystem.cpp
@@ -1,8 +1,15 @@
-﻿#include <iostream>
-#include <filesystem>
+﻿#include <experimental/filesystem>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <ctime>
+
+#include <iomanip>
+#include <sstream>
 
 
 namespace fs = std::experimental::filesystem;
+
 
 int main()
 {
@@ -10,13 +17,39 @@ int main()
     path /= "\\dir";
     std::cout << path << std::endl;  // root\dir
 
-    path /= "child";
-    std::cout << path << std::endl;  // root\dir\child
+    fs::path pathChild = path;
+    pathChild /= "child";
+    std::cout << pathChild.u8string() << std::endl;  // root\dir\child
 
     path.append("child2");
     std::cout << path << std::endl;  // root\dir\child\child2
 
     path.append("\\child3");
     std::cout << path << std::endl;  // root\dir\child\child2\child3
+
+    fs::path filename = path;
+    filename.append( "FileName.ext" );
+    std::cout << "filename: " << filename << std::endl;
+    std::cout << "filename.filename(): " << filename.filename() << std::endl;
+    std::cout << "filename.extension(): " << filename.extension() << std::endl;
+    std::cout << "filename.stem(): " << filename.stem() << std::endl;
+
+    fs::path filenameNoExt = path;
+    filenameNoExt.append("FileName");
+    std::cout << "filenameNoExt.filename(): " << filenameNoExt.filename() << std::endl;
+    std::cout << "filenameNoExt.extension(): " << filenameNoExt.extension() << std::endl;
+    std::cout << "filenameNoExt.stem(): " << filenameNoExt.stem() << std::endl;
+
+
+    fs::file_time_type filetime = fs::last_write_time("test.txt");
+    std::time_t cftime = decltype(filetime)::clock::to_time_t(filetime);
+    std::tm tm;
+    localtime_s(&tm, &cftime);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%y%m%d_%H%M%S");
+    std::cout << "filetime: " << oss.str() << std::endl;
+
+    std::cout << fs::exists("") << std::endl;
+
     return 0;
 }
